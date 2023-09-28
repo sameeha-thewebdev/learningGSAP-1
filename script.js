df7d49aea2aa2c8ba0b1654b471d0a447b773dcd
sameeha-thewebdev <140512556+sameeha-thewebdev@users.noreply.github.com>
2023-09-27 18:25:27 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+var body = document.querySelector("body");
+var crsr = document.querySelector(".crsr");
+
+body.addEventListener("mousemove", (axis) => {
+  crsr.style.display = "block"
+  crsr.style.left = axis.x + "px";
+  crsr.style.top = axis.y + "px";
+});
+
+var tl = gsap.timeline();
+
+tl.from("#nav img, h3", {
+  y: -50,
+  duration: 1,
+  delay: 0.3,
+  opacity: 0,
+  stagger: 0.5,
+});
+
+tl.from(".upper h1, p, h2", {
+  y: 50,
+  duration: 1,
+  delay: 0.3,
+  opacity: 0,
+  stagger: 0.5,
+});
+
+tl.from("#bigimg", {
+  x: 50,
+  opacity: 0,
+  duration: 1,
+});
+
+tl.from(".lower, .lower img", {
+  y: 50,
+  duration: 1,
+  delay: 0.3,
+  opacity: 0,
+  stagger: 0.5,
+});
